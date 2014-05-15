7f3aac9d84d936cdbfb37062b25fb5c20dbe2cf4
Francis Galiegue <fgaliegue@gmail.com>
2014-05-15 14:24:37 +0200

    Simplify match listeners
    Performance results don't change anyway: Guava wins; 3 times faster!

--- src/main/java/com/github/parboiled1/grappa/event/guava/GuavaMatchListener.java
+++ src/main/java/com/github/parboiled1/grappa/event/guava/GuavaMatchListener.java
@@ -2,37 +2,11 @@ package com.github.parboiled1.grappa.event.guava;
 
 import com.github.parboiled1.grappa.event.MatchEvent;
 import com.google.common.eventbus.Subscribe;
-import org.parboiled.Parboiled;
-import org.parboiled.parserunners.BasicParseRunner;
-import org.parboiled.parserunners.ParseRunner;
 
 public final class GuavaMatchListener
 {
-    private String match;
-
     @Subscribe
     public void matchEvent(final MatchEvent event)
     {
-        match = event.getMatched();
-    }
-
-    public String getMatch()
-    {
-        return match;
-    }
-
-    public static void main(final String... args)
-    {
-        final GuavaMatchListener collector = new GuavaMatchListener();
-
-        final GuavaEventBusParser parser
-            = Parboiled.createParser(GuavaEventBusParser.class, collector);
-
-        final ParseRunner<Object> runner
-            = new BasicParseRunner<>(parser.rule());
-
-        runner.run("aaaaaaaaaa");
-
-        System.out.println(collector.getMatch());
     }
 }
--- src/main/java/com/github/parboiled1/grappa/event/mbassador/MbassadorMatchListener.java
+++ src/main/java/com/github/parboiled1/grappa/event/mbassador/MbassadorMatchListener.java
@@ -2,37 +2,11 @@ package com.github.parboiled1.grappa.event.mbassador;
 
 import com.github.parboiled1.grappa.event.MatchEvent;
 import net.engio.mbassy.listener.Handler;
-import org.parboiled.Parboiled;
-import org.parboiled.parserunners.BasicParseRunner;
-import org.parboiled.parserunners.ParseRunner;
 
 public final class MbassadorMatchListener
 {
-    private String match;
-
     @Handler
     public void handleMatchEvent(final MatchEvent event)
     {
-        match = event.getMatched();
-    }
-
-    public String getMatch()
-    {
-        return match;
-    }
-
-    public static void main(final String... args)
-    {
-        final MbassadorMatchListener listener = new MbassadorMatchListener();
-
-        final MbassadorEventBusParser parser
-            = Parboiled.createParser(MbassadorEventBusParser.class, listener);
-
-        final ParseRunner<Object> runner
-            = new BasicParseRunner<>(parser.rule());
-
-        runner.run("aaaaaaaaaa");
-
-        System.out.println(listener.getMatch());
     }
 }
